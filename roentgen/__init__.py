430d1f516caf4dd7dfef8a83657f412eec5db6d9
Steven Christe <steven.d.christe@nasa.gov>
2016-03-23 11:36:01 -0700

    fix to init

--- roentgen/__init__.py
+++ roentgen/__init__.py
@@ -12,21 +12,5 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-	from __future__ import absolute_import
-
-    from roentgen.material import *
     import os
     import json
-
-_package_directory = os.path.dirname(os.path.abspath(__file__))
-_data_directory = os.path.abspath(os.path.join(_package_directory, 'data'))
-
-with open(os.path.join(_data_directory, 'elements.json')) as data_file:
-    elements_list = json.load(data_file)
-
-with open(os.path.join(_data_directory, 'compounds_mixtures.json')) as data_file:
-    compounds_mixtures_list = json.load(data_file)
-
-material_list = elements_list.copy()
-material_list.update(compounds_mixtures_list)
-
