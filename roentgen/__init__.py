2868f6fc41aa2d4367e0451bd7fdf4777f1c6380
Steven Christe <steven.d.christe@nasa.gov>
2016-03-27 20:46:59 -0500

    fixed bug in setup.py

--- roentgen/__init__.py
+++ roentgen/__init__.py
@@ -3,7 +3,8 @@
 """
 A Python package for the quantitative analysis of the interaction of energetic photons with matter (x-rays and gamma-rays).
 """
-
+import os
+import json
 # Affiliated packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
@@ -12,17 +13,14 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-    import os
-    import json
+    _package_directory = os.path.dirname(os.path.abspath(__file__))
+    _data_directory = os.path.abspath(os.path.join(_package_directory, 'data'))
 
-_package_directory = os.path.dirname(os.path.abspath(__file__))
-_data_directory = os.path.abspath(os.path.join(_package_directory, 'data'))
+    with open(os.path.join(_data_directory, 'elements.json')) as data_file:
+        elements_list = json.load(data_file)
 
-with open(os.path.join(_data_directory, 'elements.json')) as data_file:
-    elements_list = json.load(data_file)
+    with open(os.path.join(_data_directory, 'compounds_mixtures.json')) as data_file:
+        compounds_mixtures_list = json.load(data_file)
 
-with open(os.path.join(_data_directory, 'compounds_mixtures.json')) as data_file:
-    compounds_mixtures_list = json.load(data_file)
-
-material_list = elements_list.copy()
-material_list.update(compounds_mixtures_list)
+    material_list = elements_list.copy()
+    material_list.update(compounds_mixtures_list)
